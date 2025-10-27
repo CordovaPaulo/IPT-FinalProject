b4a1850ad3007ea64f7d7c03d6615d910864cb07
CordovaPaulo <202311378@gordoncollege.edu.ph>
2025-10-28 02:51:05 +0800

    learner modal and removed accesibility button

--- frontend/src/components/Providers.tsx
+++ frontend/src/components/Providers.tsx
@@ -13,7 +13,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         <RouteAnnouncer />
         {children}
         {/* Floating accessibility button + settings dialog */}
-        <AccessibilitySettings />
+        {/* <AccessibilitySettings /> */}
       </AccessibilityProvider>
     // </AuthProvider>
   );
